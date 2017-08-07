5c91834b4cfceecfbbe9cb68d8720eb88accd2dd
Ming Liu <liuming@lmws.net>
2017-08-07 15:44:25 -0700

    version 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JsonLogic.Mixfile do
   def project do
     [
       app: :json_logic,
-      version: "0.0.0",
+      version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()

8d5e6d3b63ace522bb61da6f5320122a72d0b982
Matthew Johnston <warmwaffles@gmail.com>
2023-02-16 01:54:11 -0600

    flesh out Mixfile more

--- mix.exs
+++ mix.exs
@@ -1,20 +1,17 @@
 defmodule JsonLogic.Mixfile do
   use Mix.Project
 
+  @version "0.4.0"
+
   def project do
     [
       app: :json_logic,
-      package: %{
-        description: "Elixir implementation of JsonLogic",
-        links: %{github: "https://github.com/liuming/json_logic_elixir"},
-        maintainers: ["Ming Liu"],
-        licenses: ["MIT"]
-      },
-      docs: [main: "JsonLogic", extras: ["README.md"]],
-      version: "0.4.0",
-      elixir: "~> 1.5",
+      package: package(),
+      version: @version,
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -25,7 +22,15 @@ defmodule JsonLogic.Mixfile do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp package do
+    %{
+      description: "Elixir implementation of JsonLogic",
+      links: %{github: "https://github.com/liuming/json_logic"},
+      maintainers: ["Ming Liu"],
+      licenses: ["MIT"]
+    }
+  end
+
   defp deps do
     [
       {:jason, ">= 1.0.0", optional: true},
@@ -33,6 +38,18 @@ defmodule JsonLogic.Mixfile do
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+  defp docs do
+    [
+      main: "JsonLogic",
+      extras: docs_extras(),
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/liuming/json_logic"
+    ]
+  end
+
+  defp docs_extras do
+    [
+      "README.md": [title: "Readme"]
     ]
   end
 end
